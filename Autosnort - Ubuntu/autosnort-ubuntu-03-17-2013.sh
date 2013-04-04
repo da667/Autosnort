8797ccc3dc5597cc911e2d63b858bdb750123842
da667 <deusexmachina667@gmail.com>
2013-04-03 22:30:07 -0400

    the main script was calling the child interface install scripts by the wrong name. This has been fixed.

--- Autosnort - Ubuntu/autosnort-ubuntu-03-17-2013.sh
+++ Autosnort - Ubuntu/autosnort-ubuntu-03-17-2013.sh
@@ -778,7 +778,7 @@ while true; do
 	case $ui_choice in
 		1)
 		echo "You have chosen to install Snort Report."
-		bash snortreport.sh
+		bash snortreport-ubuntu.sh
 		if [ $? != 0 ]; then
 			echo "It looks like the installation did not go as according to plan."
 			echo "Verify you have network connectiviy and try again"
@@ -791,7 +791,7 @@ while true; do
 		;;
 		2)
 		echo "You have chosen to install Aanval."
-		bash aanval.sh
+		bash aanval-ubuntu.sh
 		if [ $? != 0 ]; then
 			echo "It looks like the installation did not go as according to plan."
 			echo "Verify you have network connectiviy and try again"
